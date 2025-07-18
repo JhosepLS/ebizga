ba51517aec04aa446b63aad58da969a423a033b8
JhosepLS <jmollapazam@ulasalle.edu.pe>
2025-07-18 00:05:44 -0500

    Estudiantes Repositorio primera practica

---
+++ msestudiantescmd/msestudiantescmd/src/test/java/com/ebiz/msestudiantescmd/MsestudiantescmdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebiz.msestudiantescmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsestudiantescmdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
